3c9829a82a01b3a86b77ee5d8779fd4df1a89ebd
Taha Iqbal <tiqbal10b@gmail.com>
2023-02-13 15:56:46 -0500

    In the main application, I created models for the ToDoList and Item database objects/tables. I also set up the admin page and the superuser account as seen in the admin.png screenshot.

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# Each model corresponds to a table.
+# There is a one to many relationship from ToDoList to Item.
+class ToDoList(models.Model):   
+    name = models.CharField(max_length= 200)
+
+    def __str__(self):
+        return self.name
+
+class Item (models.Model):
+    # Belows todolist field is a foreign-key field that references a todolist.
+    todolist = models.ForeignKey(ToDoList, on_delete= models.CASCADE)
+
+    text= models.CharField(max_length=300)
+    complete= models.BooleanField()
+
+    def __str__(self):
+        return self.text
+
+
+
+
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>tech with tim</h1>")   #will return HTML 5 code
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text))) #will return HTML 5 code
 
-def v1(response):
-    return HttpResponse("<h1> view 1! </h1>")
